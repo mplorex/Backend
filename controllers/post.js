2859c1ca968e224332eccd75595791fdb4abb47b
michael posselt <michaelposselt@outlook.com>
2022-09-19 21:35:47 -0500

    backend foundation

---
+++ controllers/post.js
@@ -0,0 +1,141 @@
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const Sauce = require('../models/sauce');
+
+exports.createSauce = (req, res, next) => {
+    const data = JSON.parse(req.body.sauce)
+
+    const file = 'http://localhost:3000/' + req.file.path
+
+    const sauce = new Sauce({
+        name: data.name,
+        manufacturer: data.manufacturer,
+        description: data.description,
+        imageUrl: file,
+        mainPepper: data.mainPepper,
+        heat: data.heat,
+        userId: data.userId
+    });
+
+    sauce.save()
+        .then(() => {
+            res.status(201).json({
+                message: 'Post saved successfully!'
+            });
+        }
+        ).catch(
+            (error) => {
+                res.status(400).json(error);
+            }
+        );
+};
+
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({
+        _id: req.params.id
+    }).then(
+        (sauce) => {
+            res.status(200).json(sauce);
+        }
+    ).catch(
+        (error) => {
+            res.status(404).json(error);
+        }
+    );
+};
+
+exports.modifySauce = (req, res, next) => {
+
+    let data = req.body.sauce
+
+    if (req.body.sauce) {
+
+        data = JSON.parse(req.body.sauce)
+        data.imageUrl = 'http://localhost:3000/' + req.file.path
+    }
+
+    Sauce.updateOne({ _id: req.params.id }, data).then(
+        () => {
+            res.status(201).json({
+                message: 'Sauce updated successfully!'
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json(error);
+        }
+    );
+};
+
+exports.deleteSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id }).then(
+        (sauce) => {
+            if (!sauce) {
+                return res.status(404).json({
+                    error: new Error('Objet non trouvé !')
+                });
+            }
+            if (sauce.userId !== req.auth.userId) {
+                return res.status(401).json({
+                    error: new Error('Requête non autorisée !')
+                });
+            }
+            Sauce.deleteOne({ _id: req.params.id }).then(
+                () => {
+                    res.status(200).json({
+                        message: 'Deleted!'
+                    });
+                }
+            ).catch(
+                (error) => {
+                    res.status(400).json(error);
+                }
+            );
+        }
+    );
+};
+
+exports.getAllSauces = (req, res, next) => {
+    Sauce.find({}).then(
+        (sauces) => {
+            console.log(req.body);
+            res.status(200).json(sauces);
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json(error);
+        }
+    );
+};
+
+exports.userLikes = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id})
+        .then(sauce => {
+            const updateDoc = {}
+            const userId = req.body.userId
+
+            if (req.params.likes === 1 && !sauce.usersLiked.includes(userId)) {
+                updateDoc.$inc = { likes: 1}
+                updateDoc.$push = { usersLiked: userId }
+            } else if (req.params.dislike === -1 && !sauce.usersDisliked.includes(userId)) {
+                updateDoc.$inc = { dislike: -1}
+                updateDoc.$push = { usersDisliked: userId}
+            } else if (req.params.likes === 0) {
+                if (sauce.usersDisliked.includes(userId)) {
+                    updateDoc.$inc = {dislike: -1}
+                    updateDoc.$pull = { usersDisliked: userId}
+                } else {
+                    updateDoc.$inc = { likes: 1}
+                    updateDoc.$pull = { usersLiked: userId }
+                }
+            }
+            Sauce.updateOne({ _id: req.params.id }, updateDoc)
+                .then(() => res.status(200).json({ message: 'successful!' }))
+                .catch(
+                    (error) => {
+                        res.status(400).json(error);
+                    }
+                )
+        }) 
+} 
+
---
+++ controllers/user.js
@@ -0,0 +1,62 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/validator');
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+
+exports.signup = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10).then(
+        (hash) => {
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            });
+            user.save().then(
+                () => {
+                    res.status(201).json({
+                        message: 'User added successfully!'
+                    });
+                }
+            ).catch(
+                (error) => {
+                    console.log(error)
+                    res.status(400).json(error);
+                }
+            );
+        }
+    );
+};
+
+exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email }).then(
+        (user) => {
+            if (!user) {
+                return res.status(401).json(new Error('User not found!'));
+            }
+            bcrypt.compare(req.body.password, user.password).then(
+                (valid) => {
+                    if (!valid) {
+                        return res.status(401).json(new Error('Incorrect password!'));
+                    }
+                    const token = jwt.sign(
+                        { userId: user._id },
+                        process.env.JWT_TOKEN_SECRET,
+                        { expiresIn: '24h' });
+                    res.status(200).json({
+                        userId: user._id,
+                        token: token
+                    });
+                }
+            ).catch(
+                (error) => {
+                    console.log(error)
+                    res.status(500).json(error);
+                }
+            );
+        }
+    ).catch(
+        (error) => {
+            console.log(error)
+            res.status(500).json(error);
+        }
+    );
+}
