25f3d30cd9bee30b94ac7d458d39b84116ff63f0
michael posselt <michaelposselt@outlook.com>
2022-10-01 13:18:29 -0500

    multer changes

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -3,21 +3,17 @@ const multer = require('multer');
 const MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
-    'image/png': 'png'
+    'image/png': 'png',
+    'image/svg+xml': 'svg'
 };
 
 const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, 'images');
-    },
-    filename: (req, file, callback) => {
-        let fileSplit = file.originalname.split(' ').join('_');
-        fileSplit = fileSplit.split('.');
-        fileSplit.pop();
-        fileSplit = fileSplit.join('_');
-        const extension = MIME_TYPES[file.mimetype];
-        callback(null, fileSplit + Date.now() + '.' + extension);
-    }
+	destination: (req, file, callback) => callback(null, file.fieldname === 'profile_image' ? 'images/profiles' : 'images/posts'),
+	filename: (req, file, callback) => {
+		const name = file.originalname.split(' ').join('_');
+		const extension = MIME_TYPES[file.mimetype];
+		callback(null, name + Date.now() + '.' + extension);
+	},
 });
 
-module.exports = multer({ storage: storage }).single('image');
+module.exports = multer({ storage });
