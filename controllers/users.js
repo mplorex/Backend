8327a24baa10aa0b580e84cfc52382e905852339
michael posselt <michaelposselt@outlook.com>
2022-10-01 13:08:03 -0500

    users.js integration

---
+++ controllers/users.js
@@ -0,0 +1,18 @@
+const db = require('../config/database');
+
+exports.getAllUsers = (req, res) => {
+
+	db.query(`SELECT users.id, users.firstName, users.lastName, users.email, users.imageUrl, users.bio, users.jobTitle, users.isAdmin,
+			(SELECT COALESCE(COUNT(subscribed_user),0) FROM subscribers WHERE profile_owner = users.id AND subscriber_status = 'true') AS subscribersNum,
+			(SELECT GROUP_CONCAT(subscribed_user) FROM subscribers WHERE profile_owner = users.id AND subscriber_status = 'true') AS subscribedUsers 
+			FROM subscribers
+			JOIN users
+			WHERE users.active = 'true'
+			GROUP BY users.id
+			ORDER BY users.id ASC`, (err, result) => {
+		if (err) {
+			return res.status(500).json(err);
+		}
+		res.status(200).json(result);
+	});
+};
