09a42084ccfb2361e567e9a1d2c2a9b83a0706fb
michael posselt <michaelposselt@outlook.com>
2022-10-01 13:01:52 -0500

    post.js changes

--- controllers/post.js
+++ controllers/post.js
@@ -1,141 +1,95 @@
-const jwt = require('jsonwebtoken');
+const db = require('../config/database')
 const fs = require('fs');
-const Sauce = require('../models/sauce');
 
-exports.createSauce = (req, res, next) => {
-    const data = JSON.parse(req.body.sauce)
+exports.creatPost = (req, res) => {
+    const body = req.file ?
+        {
+            ...req.body,
+            image_url: `${req.protocol}://${req.get('host')}/images/posts/${req.file.filename}`,
+        } : { ...req.body };
 
-    const file = 'http://localhost:3000/' + req.file.path
-
-    const sauce = new Sauce({
-        name: data.name,
-        manufacturer: data.manufacturer,
-        description: data.description,
-        imageUrl: file,
-        mainPepper: data.mainPepper,
-        heat: data.heat,
-        userId: data.userId
-    });
-
-    sauce.save()
-        .then(() => {
-            res.status(201).json({
-                message: 'Post saved successfully!'
-            });
-        }
-        ).catch(
-            (error) => {
-                res.status(400).json(error);
+    const event = new Date(Date.now());
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const timestamp = event.toLocaleDateString('en-US', options);
+    db.query(`INSERT INTO posts (textual_post, image_url, creation_date, user_id) VALUES (?, ?, ?, ?)`,
+        [body.textual_post, body.image_url, timestamp, req.params.id],
+        (err, result) => { 
+            if (err) {
+                return res.status(500).json(err);
             }
-        );
-};
-
-exports.getOneSauce = (req, res, next) => {
-    Sauce.findOne({
-        _id: req.params.id
-    }).then(
-        (sauce) => {
-            res.status(200).json(sauce);
-        }
-    ).catch(
-        (error) => {
-            res.status(404).json(error);
+            res.status(201).json({ message: "Post has been saved!"})
         }
     );
 };
 
-exports.modifySauce = (req, res, next) => {
 
-    let data = req.body.sauce
-
-    if (req.body.sauce) {
-
-        data = JSON.parse(req.body.sauce)
-        data.imageUrl = 'http://localhost:3000/' + req.file.path
-    }
-
-    Sauce.updateOne({ _id: req.params.id }, data).then(
-        () => {
-            res.status(201).json({
-                message: 'Sauce updated successfully!'
-            });
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json(error);
+exports.getAllPosts = (req, res) => {
+    db.query(`SELECT users.id, users.firstName, users.lastName, users.imageUrl, isAdmin,
+                posts.post_id, posts.textual_post, posts.image_url, posts.creation_date,
+                (SELECT COALESCE(SUM(likes),0) FROM post_likes WHERE post_id = posts.post_id) AS likes, 
+                (SELECT COALESCE(SUM(dislikes),0) FROM post_likes WHERE post_id = posts.post_id) AS dislikes,
+                (SELECT COALESCE(COUNT(comment),0) FROM comments WHERE post_id = posts.post_id) AS comments FROM users
+                JOIN posts ON users.id = posts.user_id 
+                JOIN post_likes 
+                JOIN comments
+                WHERE users.active = 'true'
+                GROUP BY posts.post_id
+                ORDER BY posts.post_id DESC`,
+        (err, result) => {
+            if (err) {
+                return res.status(500).json(err);
+            }
+            let postData = [];
+            for (let i = 0; i < result.length; i++ ) {
+                postData.push(result[i]);
+            }
+        res.status(200).json(result);
         }
     );
 };
 
-exports.deleteSauce = (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id }).then(
-        (sauce) => {
-            if (!sauce) {
-                return res.status(404).json({
-                    error: new Error('Objet non trouvé !')
+exports.modifiyPost = (req, res) => {
+    db.query(`UPDATE posts SET textual_post = ? WHERE post_id = ?`,
+        [`${req.body.textual_post}`, `${req.params.id}`],
+        (err, result) => {
+            if (err) {
+                return res.status(500).json(err);
+            }
+            res.status(200).json({ message: "Post has been modified!"});
+        }
+    );
+
+};
+
+
+exports.deletePost = (req, res) => {
+    db.query(`SELECT * FROM posts WHERE post_id = ?`, req.params.id,
+        (err, postResult) => {
+            if (err) {
+                return console.log(err)
+            };
+            
+            if(postResult[0].image_ur != null ) {
+                const filename = postResult[0].image_url.split('/posts/')[1];
+                fs.unlink(`images/posts/${filename}`, () => {
+                    db.query(`DELETE FROM posts WHERE post_id = ?`, req.params.id,
+                        (err, result) => {
+                            if (err) {
+                                return res.status(500).json(err);
+                            }
+                            res.status(200).json({ message: "Post has been deleted!" });
+                        }
+                    );
                 });
-            }
-            if (sauce.userId !== req.auth.userId) {
-                return res.status(401).json({
-                    error: new Error('Requête non autorisée !')
-                });
-            }
-            Sauce.deleteOne({ _id: req.params.id }).then(
-                () => {
-                    res.status(200).json({
-                        message: 'Deleted!'
-                    });
-                }
-            ).catch(
-                (error) => {
-                    res.status(400).json(error);
-                }
-            );
-        }
-    );
-};
-
-exports.getAllSauces = (req, res, next) => {
-    Sauce.find({}).then(
-        (sauces) => {
-            console.log(req.body);
-            res.status(200).json(sauces);
-        }
-    ).catch(
-        (error) => {
-            res.status(400).json(error);
-        }
-    );
-};
-
-exports.userLikes = (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id})
-        .then(sauce => {
-            const updateDoc = {}
-            const userId = req.body.userId
-
-            if (req.params.likes === 1 && !sauce.usersLiked.includes(userId)) {
-                updateDoc.$inc = { likes: 1}
-                updateDoc.$push = { usersLiked: userId }
-            } else if (req.params.dislike === -1 && !sauce.usersDisliked.includes(userId)) {
-                updateDoc.$inc = { dislike: -1}
-                updateDoc.$push = { usersDisliked: userId}
-            } else if (req.params.likes === 0) {
-                if (sauce.usersDisliked.includes(userId)) {
-                    updateDoc.$inc = {dislike: -1}
-                    updateDoc.$pull = { usersDisliked: userId}
-                } else {
-                    updateDoc.$inc = { likes: 1}
-                    updateDoc.$pull = { usersLiked: userId }
-                }
-            }
-            Sauce.updateOne({ _id: req.params.id }, updateDoc)
-                .then(() => res.status(200).json({ message: 'successful!' }))
-                .catch(
-                    (error) => {
-                        res.status(400).json(error);
+            } else {
+                db.query(`DELETE FROM posts WHERE post_id = ?`, req.params.id,
+                    (err, result) => {
+                        if (err) {
+                            return res.status(500).json(err);
+                        }
+                        res.status(200).json({ message: "Post has been deleted!" });
                     }
-                )
-        }) 
-} 
-
+                );
+            }   
+        });
+}; 
