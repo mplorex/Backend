d899fbfeb94ec791875edc8e7ceab8d42d6c15ee
michael posselt <michaelposselt@outlook.com>
2023-01-01 14:50:14 -0600

    CRUD update for posts

--- controllers/post.js
+++ controllers/post.js
@@ -3,9 +3,7 @@ const fs = require('fs');
 const Post = require('../models/post');
 
 exports.create = (req, res, next) => { 
-    const data = JSON.parse(req.body.sauce)
-
-    
+    const data = JSON.parse(req.body.post)
 
     const file = 'http://localhost:3000/' + req.file.path
 
@@ -38,8 +36,27 @@ exports.getOne = (req, res, next) => {
     }
 };
 exports.modify = (req, res, next) => { 
+    const data = JSON.parse(req.body.post)
+    Post.update({ _id: req.params.id})
+    .then (() => {
+        res.status(201).json({
+            message: 'Post updated successfully!'
+        })
+    }).catch ((error) => {
+        res.status(400).json(error)
+    })
 };
 exports.delete = (req, res, next) => { 
+    Post.findOne({ _id: req.params.id })
+    .then((post) => {
+        Post.delete({ _id: req.params.id }).then (() => {
+            res.status(200).json({
+                message: 'Post deleted'
+            })
+        }).catch ((error) => {
+            res.status(400).json(error)
+        })
+    })
 };
 
 exports.getAll = (req, res, next) => { 
