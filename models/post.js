02d7f8b3b359555561aa2d10e036527f63afa8bf
michael posselt <michaelposselt@outlook.com>
2023-01-01 12:13:19 -0600

    post model deletion

--- models/post.js
+++ models/post.js
@@ -1,7 +1,6 @@
 const { Sequelize, Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/database.js')
 class Post extends Model {}
-const Post = require('./post.js')
 
 Post.init ({
     description: {
@@ -15,17 +14,11 @@ Post.init ({
     title: {
         type: DataTypes.STRING,
         allowNull:false
-    },
-    video: {
-        type: DataTypes.STRING,
-        allowNull:true
-    },
-    author: {
-        type: DataTypes.STRING,
-        allowNull: false
     }
 }, {
     //Other model option go here
     sequelize, // We need to pass the connection instance
     modelName: 'Post' 
 });
+
+module.exports = Post
