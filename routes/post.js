40ec4163c14aefe170204e4533237aeb77a9e5a9
michael posselt <michaelposselt@outlook.com>
2023-01-26 11:53:28 -0600

    router & post delete/update updates

--- routes/post.js
+++ routes/post.js
@@ -8,8 +8,8 @@ const postCtrl = require('../controllers/post');
 router.get('/', auth, postCtrl.getAll);
 router.post('/', auth, multer.single('file'), postCtrl.create);
 router.get('/:id', auth, postCtrl.getOne);
-router.put('/:id', auth, multer.single('file'), postCtrl.modify);
-router.delete('/:id', auth, postCtrl.delete);
+/*router.put('/:id', auth, multer.single('file'), postCtrl.modify);*/
+/*router.delete('/:id', auth, postCtrl.delete);*/
 
 
 module.exports = router;
