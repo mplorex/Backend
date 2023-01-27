730a4d83d380f90b8a14d30f9397a0a3952d9093
michael posselt <michaelposselt@outlook.com>
2023-01-26 23:57:16 -0600

    postgres login credential change

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const sequelize = new Sequelize('postgres', 'postgres', 'pizza1234', {
+const sequelize = new Sequelize('postgres', 'postgres', '#P$7hM', {
     host: 'localhost',
     dialect: 'postgres',
     operatorsAliases: false,
